b71d534e76e269e2a61e2a756a5c701a80bab27d
Alexey <info@webskill24.ru>
2020-04-16 12:07:24 +0300

    AdminProduct (2.0)

--- app/views/admin_product/index.php
+++ app/views/admin_product/index.php
@@ -10,7 +10,7 @@
                         <td class="small__width">ID товара</td>
                         <td class="name__product">Наименование</td>
                         <td class="small__width">Пол</td>
-                        <td class="small__width">ID категории</td>
+                        <td class="small__width">Категория товара</td>
                         <td class="small__width">Размер</td>
                         <td class="small__width">Цвет</td>
                         <td class="small__width">Редактировать</td>
@@ -21,30 +21,22 @@
                         <tr>
                             <td class="small__width"><?php echo $product['id']; ?></td>
                             <td class="name__product"><?php echo $product['name']; ?></td>
-                            <td class="small__width"><?php echo $product['gender']; ?></td>
-                            <td class="small__width"><?php echo $product['category_id']; ?></td>
+                            <td class="small__width"><?php if ($product['gender'] == 0) {echo 'Ж';} else {echo 'М';} ?></td>
+                            <td class="small__width">
+                                <?php if ($product['category_all_id'] == 12){echo 'Верхняя одежда';}
+                                    elseif ($product['category_all_id'] == 13){echo 'Рубашки/блузки/футболки';}
+                                    elseif ($product['category_all_id'] == 14){echo 'Свитера/толстовки';}
+                                    elseif ($product['category_all_id'] == 15){echo 'Платья/юбки';}
+                                    elseif ($product['category_all_id'] == 16){echo 'Джинсы/шорты';}
+                                    elseif ($product['category_all_id'] == 17){echo 'Аксессуары';} ?>
+                            </td>
                             <td class="small__width"><?php echo $product['size']; ?></td>
                             <td class="small__width"><?php echo $product['color']; ?></td>
                             <td class="small__width"><img src="\app\template\img\admin\eqit.svg" alt="" class='panel__icon'></td>
                             <td class="small__width"><img src="\app\template\img\admin\delite.svg" alt="" class='panel__icon'></td>
                         </tr>
                     <?php endforeach; ?>
-<!--                    <tr>-->
-<!--                        <td>2</td>-->
-<!--                        <td>Трусы</td>-->
-<!--                        <td>M</td>-->
-<!--                        <td>white</td>-->
-<!--                        <td><img src="\app\template\img\admin\eqit.svg" alt="" class='panel__icon'></td>-->
-<!--                        <td><img src="\app\template\img\admin\delite.svg" alt="" class='panel__icon'></td>-->
-<!--                    </tr>-->
-<!--                    <tr>-->
-<!--                        <td>3</td>-->
-<!--                        <td>Носки</td>-->
-<!--                        <td>L</td>-->
-<!--                        <td>green</td>-->
-<!--                        <td><img src="\app\template\img\admin\eqit.svg" alt="" class='panel__icon'></td>-->
-<!--                        <td><img src="\app\template\img\admin\delite.svg" alt="" class='panel__icon'></td>-->
-<!--                    </tr>-->
+
             </table>
         </div>
     </section>

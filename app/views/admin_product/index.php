e1e655c421b4d1d2e2d1ac3651f769e59759c55f
slavahawk <krylov_s10@mail.ru>
2020-04-16 14:43:41 +0700

    пошалил и все сделал

--- app/views/admin_product/index.php
+++ app/views/admin_product/index.php
@@ -1,48 +1,32 @@
 <?php include ROOT . '/app/views/layouts/header_admin.php'; ?>
 
-<main class="admin__item">
-    <section class="admin-welcome">
-        <div class="admin-welcome__title">Меню</div>
-        <div class="admin-welcome-subtitle">
-            <ul>
-                <li>
-                    <a href="#">Управление товарами (Бета)</a>
-                </li>
-                <li>
-                    <a>Управление категориями (Бета)</a>
-                </li>
-                <li>
-                    <a>Управление заказами (Бета)</a>
-                </li>
-            </ul>
-        </div>
-    </section>
+<main>
     <section class="admin-panel">
         <div class="panel__title">Управление товарами(Бета)</div>
         <div class="panel__item">
             <div class="pamel__item-title">Список товаров</div>
             <table class="panet__table">
                     <tr>
-                        <td>ID товара</td>
-                        <td>Наименование</td>
-                        <td>Пол</td>
-                        <td>ID категории</td>
-                        <td>Размер</td>
-                        <td>Цвет</td>
-                        <td>Редактировать</td>
-                        <td>Удалить</td>
+                        <td class="small__width">ID товара</td>
+                        <td class="name__product">Наименование</td>
+                        <td class="small__width">Пол</td>
+                        <td class="small__width">ID категории</td>
+                        <td class="small__width">Размер</td>
+                        <td class="small__width">Цвет</td>
+                        <td class="small__width">Редактировать</td>
+                        <td class="small__width">Удалить</td>
                     </tr>
 
                     <?php foreach ($productList as $product): ?>
                         <tr>
-                            <td><?php echo $product['id']; ?></td>
-                            <td><?php echo $product['name']; ?></td>
-                            <td><?php echo $product['gender']; ?></td>
-                            <td><?php echo $product['category_id']; ?></td>
-                            <td><?php echo $product['size']; ?></td>
-                            <td><?php echo $product['color']; ?></td>
-                            <td><img src="\app\template\img\admin\eqit.svg" alt="" class='panel__icon'></td>
-                            <td><img src="\app\template\img\admin\delite.svg" alt="" class='panel__icon'></td>
+                            <td class="small__width"><?php echo $product['id']; ?></td>
+                            <td class="name__product"><?php echo $product['name']; ?></td>
+                            <td class="small__width"><?php echo $product['gender']; ?></td>
+                            <td class="small__width"><?php echo $product['category_id']; ?></td>
+                            <td class="small__width"><?php echo $product['size']; ?></td>
+                            <td class="small__width"><?php echo $product['color']; ?></td>
+                            <td class="small__width"><img src="\app\template\img\admin\eqit.svg" alt="" class='panel__icon'></td>
+                            <td class="small__width"><img src="\app\template\img\admin\delite.svg" alt="" class='panel__icon'></td>
                         </tr>
                     <?php endforeach; ?>
 <!--                    <tr>-->

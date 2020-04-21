4b9078095590753be69d4dfe24aa84b1b6b0e758
slavahawk <krylov_s10@mail.ru>
2020-04-21 14:24:01 +0700

    исправил phone

--- app/template/js/main.js
+++ app/template/js/main.js
@@ -257,4 +257,7 @@ function mask(e) {
 }
 window.addEventListener('DOMContentLoaded', function () {
   let input = document.querySelector('.online_phone');
+  input.addEventListener('input', mask, false);
+  input.focus(true);
+  setCursorPosition(3, input);
 });

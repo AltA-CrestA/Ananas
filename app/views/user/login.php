233930831fff17f84339dceb65d18a6f5d912a7a
slavahawk <krylov_s10@mail.ru>
2020-07-01 03:33:43 +0700

    login and singUp

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -1,10 +1,10 @@
 <?php include ROOT . '/app/views/layouts/header.php'; ?>
 
-<section class="login-content">
-    <div class="login-container">
-        <div class="login-top">
-            <div class="login-title">Войти в личный кабинет</div>
-            <div class="login-text">Нет личного кабинета?
+<section class="login">
+    <div class="login__content">
+        <div class="login__content-top">
+            <div class="login__content-top-title">Войти в личный кабинет</div>
+            <div class="login__content-top-text">Нет личного кабинета?
                 <a href="/user/register/" class="login__link">Зарегистрируйтесь</a>
             </div>
         </div>
@@ -17,24 +17,24 @@
             </ul>
         <?php endif; ?>
 
-        <form class="login-form" method="POST">
-            <div class="login__string">
-                <label class="label-login"><strong>Логин</strong>&nbsp;:</label>
-                <input class="login__input" type="text" name="login" value="<?php echo @$_POST['login']; ?>">
+        <form class="login__content-form" method="POST">
+            <div class="login__content-form-item">
+                <label><strong>Логин</strong>&nbsp;:</label>
+                <input type="text" name="login" value="<?php echo @$_POST['login']; ?>">
             </div>
-            <div class="login__string">
-                <label class="label-login"><strong>Пароль</strong>&nbsp;:</label>
-                <input class="login__input" type="password" name="password">
+            <div class="login__content-form-item">
+                <label><strong>Пароль</strong>&nbsp;:</label>
+                <input type="password" name="password">
             </div>
-            <div class="checkbox__login">
-                <label class="checkbox">
-                    <input class="checkbox__input" type="checkbox" name="remember" value="1">
+            <div class="login__content-form-checkbox">
+                <label>
+                    <input type="checkbox" name="remember" value="1">
                     <div class="checkbox__div"></div>
                 </label>
                 <p>Запомнить меня</p>
             </div>
             <div id="active" class="login-bottom">
-                <button class="button__login" type="submit" name="do_login">Войти</button>
+                <button type="submit" name="do_login">Войти</button>
             </div>
         </form>
     </div>
